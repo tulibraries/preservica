c0d19e0985299e032944341f520aeafe0ae6579a
htomren <htomren@gmail.com>
2024-08-12 14:26:41 -0400

    update comment

--- preservica_entities/update_asset_from_txt.py
+++ preservica_entities/update_asset_from_txt.py
@@ -1,7 +1,7 @@
 # This script updates metadata for assets in Preservica based on input from a tab-delimited text file
 # To use this script, create a tab-delmited text file called "update_asset.txt" and save it in the same directory from which you are running this script
 # The text file should have 3 columns, with the following headers: asset_reference, new_description, and new_identifier
-# - folder_reference should contain the Preservica xip.reference of the asset to be updated
+# - asset_reference should contain the Preservica xip.reference of the asset to be updated
 # - new_description should contain the desired value of the xip.description field
 # - new_identifier should contain the desired value of the identifier (type=code) field (generally this will be the same value as xip.description)
 # You will also need the XML metadata file "LegacyXIP_accessionRef_catalog.xml" in the same directory from which you are running this script
